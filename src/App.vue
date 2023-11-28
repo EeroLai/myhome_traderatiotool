db2e6bb409fb65f44ea59fad2c807c296c51b721
EeroLai <84887419+EeroLai@users.noreply.github.com>
2023-11-28 21:30:39 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import data from "./assets/data.json";
+</script>
+
+<template>
+  <div>
+    <select id="fruitDropdown">
+      <!-- 这里将会动态添加选项 -->
+    </select>
+  </div>
+</template>
+
+<style scoped></style>
